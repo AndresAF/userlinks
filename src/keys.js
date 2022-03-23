3c54dd8c5f728444766e8de2964d50ba06214810
DESKTOP-SU7N3I6\USUARIO <andresagfa@outlook.com>
2022-03-22 23:04:26 -0600

    no message

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,9 @@
 module.exports = {
     database: {
-        host: "localhost",
-        user: "root",
-        password: "123456",
-        database: "database_links"
+        host: 'us-cdbr-east-05.cleardb.net',
+        user:'b2aff2868c0c16',
+        password:'1454ff90',
+        port: 3306,
+        database: 'heroku_3d5759d60390468'
     }
 }
